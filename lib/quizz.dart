57b489373a667e4c5343ed0523f5c474cafc51c8
Larissa-Sanar <sanar@MacBook-Pro-de-Sanar.local>
2020-05-19 20:19:34 -0300

    feat: add next question logic

---
+++ lib/quizz.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> score = [];
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
+  int actualQuestion = 0;
+
+  List<bool> questionsAnswers = [false, true, true];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                questions[actualQuestion],
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  bool isAnswerCorrect =
+                      questionsAnswers[actualQuestion] == true;
+
+                  if (isAnswerCorrect) {
+                    print('Correct');
+                  } else {
+                    print('False');
+                  }
+
+                  actualQuestion++;
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  bool isAnswerCorrect =
+                      questionsAnswers[actualQuestion] == false;
+
+                  if (isAnswerCorrect) {
+                    print('Correct');
+                  } else {
+                    print('False');
+                  }
+
+                  actualQuestion++;
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: score,
+        )
+      ],
+    );
+  }
+}
